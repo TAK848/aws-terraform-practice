34d6f99fcf2ab42cde1f78dc5dd1b7dde41cbff5
TAK848 <pairs_char0j@icloud.com>
2022-11-02 15:01:09 +0900

    create template

--- main.tf
+++ main.tf
@@ -1,19 +1,31 @@
+# -------------------------------
+# terraform configuration
+# -------------------------------
+
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+  }
+}
+
+# -------------------------------
+# provider
+# -------------------------------
 provider "aws" {
   profile = "terraform"
   region  = "ap-northeast-1"
 }
 
-resource "aws_instance" "hello-world" {
-  ami           = "ami-0de5311b2a443fb89"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-
-  user_data = <<-EOF
-#!/bin/bash
-amazon-linux-extras install nginx1.12 -y
-systemctl start nginx
-EOF
+# -------------------------------
+# variables
+# -------------------------------
+variable "project" {
+  type = string
+}
+variable "environment" {
+  type = string
 }
