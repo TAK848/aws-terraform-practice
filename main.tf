9f320c85781a62400a3b22bf43e0d19bd5f13cc2
TAK848 <pairs_char0j@icloud.com>
2022-11-03 13:59:45 +0900

    elb-https + virginia acm

--- main.tf
+++ main.tf
@@ -25,6 +25,11 @@ provider "aws" {
   profile = "terraform"
   region  = "ap-northeast-1"
 }
+provider "aws" {
+  alias   = "virginia"
+  profile = "terraform"
+  region  = "us-east-1"
+}
 
 # -------------------------------
 # variables
