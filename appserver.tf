f17ff0df8adfea4b1a060a0e1c6fb0d7af6e450b
TAK848 <pairs_char0j@icloud.com>
2022-11-03 17:58:00 +0900

    autoscailing

--- appserver.tf
+++ appserver.tf
@@ -40,27 +40,147 @@ resource "aws_ssm_parameter" "password" {
 }
 
 
-# -------------------------------
-# EC2 Instance
-# -------------------------------
-resource "aws_instance" "app_server" {
-  ami                         = data.aws_ami.app.id
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.public_subnet_1a.id
-  associate_public_ip_address = true
+# # -------------------------------
+# # EC2 Instance
+# # -------------------------------
+# resource "aws_instance" "app_server" {
+#   ami                         = data.aws_ami.app.id
+#   instance_type               = "t2.micro"
+#   subnet_id                   = aws_subnet.public_subnet_1a.id
+#   associate_public_ip_address = true
 
-  iam_instance_profile = aws_iam_instance_profile.app_ec2_profile.name
+#   iam_instance_profile = aws_iam_instance_profile.app_ec2_profile.name
 
-  vpc_security_group_ids = [
-    aws_security_group.app_sg.id,
-    aws_security_group.opmng_sg.id
-  ]
+#   vpc_security_group_ids = [
+#     aws_security_group.app_sg.id,
+#     aws_security_group.opmng_sg.id
+#   ]
+#   key_name = aws_key_pair.keypair.key_name
+
+#   tags = {
+#     Name    = "${var.project}-${var.environment}-app-ec2"
+#     Project = var.project
+#     Env     = var.environment
+#     Type    = "app"
+#   }
+# }
+
+# -------------------------------
+# launch template
+# -------------------------------
+resource "aws_launch_template" "app_lt" {
+  update_default_version = true
+
+  name = "${var.project}-${var.environment}-app-lt"
+
+  image_id = data.aws_ami.app.id
+
   key_name = aws_key_pair.keypair.key_name
 
-  tags = {
-    Name    = "${var.project}-${var.environment}-app-ec2"
-    Project = var.project
-    Env     = var.environment
-    Type    = "app"
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name    = "${var.project}-${var.environment}-app-ec2"
+      Project = var.project
+      Env     = var.environment
+      Type    = "app"
+    }
   }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups = [
+      aws_security_group.app_sg.id,
+      aws_security_group.opmng_sg.id,
+    ]
+    delete_on_termination = true
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.app_ec2_profile.name
+  }
+
+  user_data = filebase64("./src/initialize.sh")
+}
+
+# -------------------------------
+# auto scaling group
+# -------------------------------
+resource "aws_autoscaling_group" "app_asg" {
+  name = "${var.project}-${var.environment}-app-asg"
+
+  max_size         = 3
+  min_size         = 1
+  desired_capacity = 1
+
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+
+  vpc_zone_identifier = [
+    aws_subnet.public_subnet_1a.id,
+    aws_subnet.public_subnet_1c.id,
+  ]
+
+  target_group_arns = [
+    aws_lb_target_group.alb_target_group.arn,
+  ]
+
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = aws_launch_template.app_lt.id
+        version            = "$Latest"
+      }
+
+      override {
+        instance_type = "t2.micro"
+      }
+    }
+  }
+}
+resource "aws_autoscaling_policy" "app_asg_policy_out" {
+  name                   = "${var.project}-${var.environment}-app-asg-policy-out"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.app_asg.name
+}
+resource "aws_autoscaling_policy" "app_asg_policy_in" {
+  name                   = "${var.project}-${var.environment}-app-asg-policy-in"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.app_asg.name
+}
+resource "aws_cloudwatch_metric_alarm" "app_asg_alarm_high" {
+  alarm_name          = "${var.project}-${var.environment}-app-asg-alarm-high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "This metric monitors ec2 cpu utilization"
+  alarm_actions       = [aws_autoscaling_policy.app_asg_policy_out.arn]
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.app_asg.name
+  }
+  treat_missing_data = "breaching"
+}
+resource "aws_cloudwatch_metric_alarm" "app_asg_alarm_low" {
+  alarm_name          = "${var.project}-${var.environment}-app-asg-alarm-low"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "50"
+  alarm_description   = "This metric monitors ec2 cpu utilization"
+  alarm_actions       = [aws_autoscaling_policy.app_asg_policy_in.arn]
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.app_asg.name
+  }
+  treat_missing_data = "breaching"
 }
--- data.tf
+++ data.tf
@@ -7,14 +7,18 @@ data "aws_ami" "app" {
   owners      = ["self", "amazon"]
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.*-x86_64-gp2"]
+    values = ["testprj-app-ami"]
   }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+  # filter {
+  #   name   = "name"
+  #   values = ["amzn2-ami-kernel-5.10-hvm-2.0.*-x86_64-gp2"]
+  # }
+  # filter {
+  #   name   = "root-device-type"
+  #   values = ["ebs"]
+  # }
+  # filter {
+  #   name   = "virtualization-type"
+  #   values = ["hvm"]
+  # }
 }
